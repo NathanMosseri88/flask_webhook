e40d891be25d399e9bcaa385537384919b4423a0
jacob <jakeytawil18@gmail.com>
2024-02-01 16:43:46 -0500

    added ngrok download if not installed

--- Batch Files/configure.bat
+++ Batch Files/configure.bat
@@ -10,6 +10,15 @@ call activate.bat
 cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Nathan's\delegation_webhook"
 pip install -r requirements.txt
 
+IF EXIST "C:\Users\%USERNAME%\Desktop\ngrok-v3-stable-windows-amd64\ngrok.exe" (
+    echo ngrok is already installed
+) ELSE (
+    echo Downloading ngrok
+    curl -0 https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip
+    unzip ngrok-stable-windows-amd64.zip -d C:\Users\%USERNAME%\Desktop\
+    echo ngrok downloaded
+)
+
 start cmd /k "C:\Users\%USERNAME%\Desktop\ngrok-v3-stable-windows-amd64\ngrok.exe start --config="C:\\Users\\%USERNAME%\\Desktop\\New Projects\\Nathan's\\delegation_webhook\\ngrok.yml" delegation"
 
 cd "Python Files"
