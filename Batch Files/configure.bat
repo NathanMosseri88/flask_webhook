e101138a855d57bb70cc72bd53e755eb46621c77
jacob <jakeytawil18@gmail.com>
2024-02-01 17:15:11 -0500

    create ngrok.yml thru batch

--- Batch Files/configure.bat
+++ Batch Files/configure.bat
@@ -19,7 +19,16 @@ IF EXIST "C:\Users\%USERNAME%\Desktop\ngrok-v3-stable-windows-amd64\ngrok.exe" (
     echo ngrok downloaded
 )
 
-start cmd /k "C:\Users\%USERNAME%\Desktop\ngrok-v3-stable-windows-amd64\ngrok.exe start --config="C:\\Users\\%USERNAME%\\Desktop\\New Projects\\Nathan's\\delegation_webhook\\ngrok.yml" delegation"
+cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Nathan's\delegation_webhook"
+echo version: "2" > ngrok.yml
+echo authtoken: "1exTA3FUMpg1OOC6vR5sA2PVtk8_2raxh83uKPB57CfSf5jdh" >> ngrok.yml
+echo tunnels: >> ngrok.yml
+echo  %COMPUTERNAME%: >> ngrok.yml
+echo   proto: http >> ngrok.yml
+echo   addr: 5000 >> ngrok.yml
+echo   domain: easy-macaw-nearly.ngrok-free.app >> ngrok.yml
+
+start cmd /k "C:\Users\%USERNAME%\Desktop\ngrok-v3-stable-windows-amd64\ngrok.exe start --config="C:\\Users\\%USERNAME%\\Desktop\\New Projects\\Nathan's\\delegation_webhook\\ngrok.yml" %COMPUTERNAME%"
 
 cd "Python Files"
 start cmd /k "python app.py"
