290b8a3e1ef03972181fe9346676c86dc482c005
jacob <jakeytawil18@gmail.com>
2024-02-02 10:11:09 -0500

    separating batch

--- Batch Files/run_webhook.bat
+++ Batch Files/run_webhook.bat
@@ -1,4 +1,13 @@
 cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Nathan's\delegation_webhook"
+echo version: "2" > ngrok.yml
+echo authtoken: "1exTA3FUMpg1OOC6vR5sA2PVtk8_2raxh83uKPB57CfSf5jdh" >> ngrok.yml
+echo tunnels: >> ngrok.yml
+echo  %COMPUTERNAME%: >> ngrok.yml
+echo   proto: http >> ngrok.yml
+echo   addr: 5000 >> ngrok.yml
+echo   domain: easy-macaw-nearly.ngrok-free.app >> ngrok.yml
+
+cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Nathan's\delegation_webhook"
 start cmd /k "C:\Users\%USERNAME%\Desktop\ngrok-v3-stable-windows-amd64\ngrok.exe start --config="C:\\Users\\%USERNAME%\\Desktop\\New Projects\\Nathan's\\delegation_webhook\\ngrok.yml" %COMPUTERNAME%"
 
 cd "Python Files"
